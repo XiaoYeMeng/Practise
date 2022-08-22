54819ec0ad45ef5ffb95ccf85c7b85b5d9999dd1
YUYU <airimid0125@gmail.com>
2022-08-22 11:20:42 +0800

    update0822

--- Practise/ver_4_FastAPI.py
+++ Practise/ver_4_FastAPI.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 import openpyxl
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
 # 讀取excel sheet
-xlsx = openpyxl.load_workbook('./menu.xlsx')
+xlsx = openpyxl.load_workbook('./menu/menu.xlsx')
 sheet = xlsx['menu']
 # 第一題
 beef = sheet.cell(column=1, row=2).value
@@ -19,7 +20,7 @@ menu = {
 }
 
 
-@app.post("/")
+@app.post('/')
 # 更新菜單
 async def root(i: str):
     global menu
@@ -27,30 +28,48 @@ async def root(i: str):
     if i == 'yuan_qi_beef_+1' and menu['beef'] >= 0:
         menu['beef'] += 1
         sheet['A2'] = menu['beef']
-        xlsx.save('./menu.xlsx')
+        xlsx.save('./menu/menu.xlsx')
 
     # 如果是 XX-1 品項庫存-1
     elif i == 'yuan_qi_beef_-1' and menu['beef'] > 0:
         menu['beef'] -= 1
         sheet['A2'] = menu['beef']
-        xlsx.save('./menu.xlsx')
+        xlsx.save('./menu/menu.xlsx')
 
     elif i == 'yuan_qi_pork_+1' and menu['pork'] >= 0:
         menu['pork'] += 1
         sheet['B2'] = menu['pork']
-        xlsx.save('./menu.xlsx')
+        xlsx.save('./menu/menu.xlsx')
 
     elif i == 'yuan_qi_pork_-1' and menu['pork'] > 0:
         menu['pork'] -= 1
         sheet['B2'] = menu['pork']
-        xlsx.save('./menu.xlsx')
+        xlsx.save('./menu/menu.xlsx')
 
     elif i == 'yuan_qi_lamb_+1' and menu['lamb'] >= 0:
         menu['lamb'] += 1
         sheet['C2'] = menu['lamb']
-        xlsx.save('./menu.xlsx')
+        xlsx.save('./menu/menu.xlsx')
 
     elif i == 'yuan_qi_lamb_-1' and menu['lamb'] > 0:
         menu['lamb'] -= 1
         sheet['C2'] = menu['lamb']
-        xlsx.save('./menu.xlsx')
+        xlsx.save('./menu/menu.xlsx')
+
+
+@app.get('/image1/')
+async def image():
+    filename = './images/1.png'
+    return FileResponse(filename, filename='fastapi_1.png')
+
+
+@app.get('/image2/')
+async def image():
+    filename = './images/2.png'
+    return FileResponse(filename, filename='fastapi_2.png')
+
+
+@app.get('/image3/')
+async def image():
+    filename = './images/3.png'
+    return FileResponse(filename, filename='fastapi_3.png')
--- Practise/ver_4_Tk.py
+++ Practise/ver_4_Tk.py
@@ -3,40 +3,54 @@ import requests
 
 window = tk.Tk()
 window.title('QRCode')
-window.geometry('1000x600')
+window.geometry('500x300')
+
+url = 'http://127.0.0.1:8888/?i=yuan_qi_'
 
 
+# 菜單更新
 def menu_update(i):
+    # 牛肉庫存 + 1
     if i == 'yuan_qi_beef_%2B1':
-        requests.post('http://127.0.0.1:8888/?i=yuan_qi_beef_%2B1')
+        requests.post(url + 'beef_%2B1')
 
+    # 牛肉庫存 - 1
     if i == 'yuan_qi_beef_-1':
-        requests.post('http://127.0.0.1:8888/?i=yuan_qi_beef_-1')
+        requests.post(url + 'beef_-1')
 
     if i == 'yuan_qi_pork_%2B1':
-        requests.post('http://127.0.0.1:8888/?i=yuan_qi_pork_%2B1')
+        requests.post(url + 'pork_%2B1')
 
     if i == 'yuan_qi_pork_-1':
-        requests.post('http://127.0.0.1:8888/?i=yuan_qi_pork_-1')
+        requests.post(url + 'pork_-1')
 
     if i == 'yuan_qi_lamb_%2B1':
-        requests.post('http://127.0.0.1:8888/?i=yuan_qi_lamb_%2B1')
+        requests.post(url + 'lamb_%2B1')
 
     if i == 'yuan_qi_lamb_-1':
-        requests.post('http://127.0.0.1:8888/?i=yuan_qi_lamb_-1')
+        requests.post(url + 'lamb_-1')
 
 
-bt = tk.Button(window, text='牛肉庫存數量 + 1', width=18, command=lambda: menu_update('yuan_qi_beef_%2B1'), font=('Time new roman', 18))
+'''
+GUI
+'''
+bt = tk.Button(window, text='牛肉庫存數量 + 1', width=18, command=lambda: menu_update('yuan_qi_beef_%2B1'),
+               font=('Time new roman', 18))
 bt.pack()
-bt = tk.Button(window, text='牛肉庫存數量 - 1', width=18, command=lambda: menu_update('yuan_qi_beef_-1'), font=('Time new roman', 18))
+bt = tk.Button(window, text='牛肉庫存數量 - 1', width=18, command=lambda: menu_update('yuan_qi_beef_-1'),
+               font=('Time new roman', 18))
 bt.pack()
-bt = tk.Button(window, text='豬肉庫存數量 + 1', width=18, command=lambda: menu_update('yuan_qi_pork_%2B1'), font=('Time new roman', 18))
+bt = tk.Button(window, text='豬肉庫存數量 + 1', width=18, command=lambda: menu_update('yuan_qi_pork_%2B1'),
+               font=('Time new roman', 18))
 bt.pack()
-bt = tk.Button(window, text='豬肉庫存數量 - 1', width=18, command=lambda: menu_update('yuan_qi_pork_-1'), font=('Time new roman', 18))
+bt = tk.Button(window, text='豬肉庫存數量 - 1', width=18, command=lambda: menu_update('yuan_qi_pork_-1'),
+               font=('Time new roman', 18))
 bt.pack()
-bt = tk.Button(window, text='羊肉庫存數量 + 1', width=18, command=lambda: menu_update('yuan_qi_lamb_%2B1'), font=('Time new roman', 18))
+bt = tk.Button(window, text='羊肉庫存數量 + 1', width=18, command=lambda: menu_update('yuan_qi_lamb_%2B1'),
+               font=('Time new roman', 18))
 bt.pack()
-bt = tk.Button(window, text='羊肉庫存數量 - 1', width=18, command=lambda: menu_update('yuan_qi_lamb_-1'), font=('Time new roman', 18))
+bt = tk.Button(window, text='羊肉庫存數量 - 1', width=18, command=lambda: menu_update('yuan_qi_lamb_-1'),
+               font=('Time new roman', 18))
 bt.pack()
 
 window.mainloop()
